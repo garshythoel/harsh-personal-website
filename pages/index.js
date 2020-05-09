3a59dda8bb4faca4e51b08f530f94dc49d0745ca
Harshyt Goel <harshyt.goel@gmail.com>
2020-05-08 20:59:47 -0700

    Prettier + eslint config

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,23 @@
-import { GALink, initGA, logEvent, logPageView } from '../utilities/analytics'
+import { GALink, initGA, logPageView } from '../utilities/analytics';
 
-import Head from 'next/head'
-import { useEffect } from 'react'
+import Head from 'next/head';
+import { useEffect } from 'react';
 
-export default function Home () {
+export default function Home() {
   useEffect(() => {
     if (!window.GA_INITIALIZED) {
-      initGA()
-      window.GA_INITIALIZED = true
+      initGA();
+      window.GA_INITIALIZED = true;
     }
 
-    logPageView()
-  }, [])
+    logPageView();
+  }, []);
 
   return (
     <>
       <Head>
         <title>Hg</title>
-        <meta
-          name="description"
-          content="I am an individual."
-        />
+        <meta name="description" content="I am an individual." />
         <meta name="language" content="English" />
         <meta name="googlebot" content="index,follow" />
         <link rel="shortcut icon" href="/sakura.png" />
@@ -37,10 +34,11 @@ export default function Home () {
                 target="_blank"
                 to="https://harsh.substack.com"
               >
-              creativity, inspiration and growth.
+                creativity, inspiration and growth.
               </GALink>
             </p>
-            <p>Here are my&nbsp;
+            <p>
+              Here are my&nbsp;
               <GALink
                 eventLabel="Clicked Rules To Life"
                 rel="noopener noreferrer"
@@ -66,7 +64,8 @@ export default function Home () {
                 eventLabel="Clicked Loom"
                 target="_blank"
                 rel="noopener noreferrer"
-                to="https://loom.com/">
+                to="https://loom.com/"
+              >
                 loom.com
               </GALink>
             </p>
@@ -76,11 +75,15 @@ export default function Home () {
                 eventLabel="Clicked Twitter"
                 target="_blank"
                 rel="noopener noreferrer"
-                to="https://twitter.com/garshythoel">
+                to="https://twitter.com/garshythoel"
+              >
                 Twitter@garshythoel
               </GALink>
             </p>
-            <p>For long form thoughts on everything I&apos;ve mentioned, hit that subscribe button:</p>
+            <p>
+              For long form thoughts on everything I&apos;ve mentioned, hit that
+              subscribe button:
+            </p>
             <GALink
               eventLabel="Clicked Substack Button"
               className="subscribe-button"
@@ -90,7 +93,10 @@ export default function Home () {
             >
               Subscribe
             </GALink>
-            <p className="textSize-s">(You will get an email once <i>every 2-ish weeks</i>, if inspiration has been abundant. That&apos;s wild right?)</p>
+            <p className="textSize-s">
+              (You will get an email once <i>every 2-ish weeks</i>, if
+              inspiration has been abundant. That&apos;s wild right?)
+            </p>
           </div>
 
           {/* <Tweet /> */}
@@ -100,5 +106,5 @@ export default function Home () {
         </div>
       </div>
     </>
-  )
+  );
 }
